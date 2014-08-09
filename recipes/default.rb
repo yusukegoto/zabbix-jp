c0e2344471c042f3e1ba3bda5856c70ea80cbfc8
Yusuke Goto <u.suke.goto@gmail.com>
2014-08-10 01:14:05 +0900

    epel

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,12 @@
 #
 include_recipe 'localedef'
 
+yum_repository 'epel' do
+  description 'Extra Packages for Enterprise Linux'
+  mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
+  gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
+end
+
 yum_repository 'zabbix-release-2.2-1.el6.noarch.rpm' do
   baseurl 'http://repo.zabbix.com/zabbix/2.2/rhel/6/x86_64/'
   gpgkey 'http://repo.zabbix.com/RPM-GPG-KEY-ZABBIX'
