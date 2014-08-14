c09035d7e68ba8b849b5323c86417e3a415c5dd9
Yusuke Goto <u.suke.goto@gmail.com>
2014-08-15 01:10:50 +0900

    fix agent config

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,5 @@
-default['zabbix'][:agent][:server]   = '127.0.0.1'
-default['zabbix'][:agent][:hostname] = node.ipaddress
+default[:zabbix][:agent][:hostname] = node.ipaddress
+
+default[:zabbix][:agent][:server]        = '127.0.0.1'
+default[:zabbix][:agent][:server_active] = ''
+
